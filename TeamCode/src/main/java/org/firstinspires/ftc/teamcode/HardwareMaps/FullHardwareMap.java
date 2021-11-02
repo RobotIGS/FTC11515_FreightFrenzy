86a2271466a6a0e8724dadf6c25b362f93644713
Ember 'n0emis' Keske <git@n0emis.eu>
2021-11-02 17:10:09 +0100

    fix hwMap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMaps/FullHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMaps/FullHardwareMap.java
@@ -7,9 +7,9 @@ public class FullHardwareMap extends BaseHardwareMap {
     @Override
     public void init(HardwareMap hwMap) {
         motor_front_left = hwMap.get(DcMotor.class, "hub1_motorport0");
-        motor_front_right = hwMap.get(DcMotor.class, "hub1_motorport1");
-        motor_rear_left = hwMap.get(DcMotor.class, "hub1_motorport2");
-        motor_rear_right = hwMap.get(DcMotor.class, "hub1_motorport3");
+        motor_front_right = hwMap.get(DcMotor.class, "hub1_motorport2");
+        motor_rear_left = hwMap.get(DcMotor.class, "hub1_motorport3");
+        motor_rear_right = hwMap.get(DcMotor.class, "hub1_motorport1");
 
         motor_front_left.setDirection(DcMotor.Direction.FORWARD);
         motor_front_right.setDirection(DcMotor.Direction.REVERSE);
