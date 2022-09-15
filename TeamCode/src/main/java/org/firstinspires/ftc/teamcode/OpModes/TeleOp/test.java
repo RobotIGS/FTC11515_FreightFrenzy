590f2a5895d0ce9bed7309f9b6f4503705a5f95b
RobotIGS Laptop <robot.igs@igs-goettingen.de>
2022-09-15 16:59:32 +0200

    test optimiert

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/test.java
@@ -14,18 +14,28 @@ public class test extends BaseTeleOp {
     @Override
     public void loop() {
         if (gamepad1.a){
-            robot.motor_rear_right.setPower(-0.7);
-            robot.motor_front_right.setPower(-0.7);
-            robot.motor_rear_right.setPower(-0.7);
-            robot.motor_front_right.setPower(-0.7);
-
-
+            robot.motor_rear_right.setPower(-0.4);
+            robot.motor_front_right.setPower(-0.4);
+            robot.motor_rear_left.setPower(0.4);
+            robot.motor_front_left.setPower(0.4);
+        }
+        else if (gamepad1.y){
+            robot.motor_rear_right.setPower(0.7);
+            robot.motor_front_right.setPower(0.7);
+            robot.motor_rear_left.setPower(-0.7);
+            robot.motor_front_left.setPower(-0.7);
+        }
+        else if (gamepad1.x){
+            robot.motor_front_left.setPower(0.4);
+            robot.motor_rear_left.setPower(0.6);
         }
         else if (gamepad1.b){
-            robot.motor_front_right.setPower(0.6);
-            robot.motor_rear_right.setPower(0.4);
+            robot.motor_front_right.setPower(-0.6);
+            robot.motor_rear_right.setPower(-0.4);
         }
-        );
+        else {
+            omniWheel.setMotors(0,0,0);
+        }
     }
 }
 
